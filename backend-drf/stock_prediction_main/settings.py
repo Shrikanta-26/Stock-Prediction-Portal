b2bb5374eec638aa2dbd671d8c9e1f4e117dfdfd
shrikanta <shrikantadutta108@gmail.com>
2026-01-21 13:31:13 +0600

    Adding axios functionality in frontend and conncect with db

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'api',
+    'corsheaders',
 ]
 AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOWED_ORIGINS = [
+  
+'http://localhost:5173'
+]
--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 const Register = () => {
+
+const [username, setUsername] = useState('')
+const [email, setEmail] = useState('')
+const [password,setPassword] = useState('')
+
+
+const handleRegistration=async (e) =>{
+  e.preventDefault();
+ 
+  const userData = {
+    username,email,password
+  }
+
+  try{
+    const response = await  axios.post('http://127.0.0.1:8000/api/v1/register/',userData)
+    console.log('response.data=> ' ,response.data)
+    console.log('Registration Successful')
+  }catch(error){
+    console.error('Registration error: ',error.response.data)
+  }
+
+}
+
   return (
-    <div className='text-light'>Register</div>
-  )
+<>
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-6 bg-light-dark p-5 rounded">
+        <h3 className='text-light text-center mb-4'>Create an Account</h3>
+      <form onSubmit={handleRegistration}>
+        <input type="text" className='form-control mb-3' placeholder='Enter username' value={username}  onChange={(e)=> setUsername(e.target.value)}/>
+        <input type="email" className='form-control mb-3' placeholder='Enter email address' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        <input type="password" className='form-control mb-5' placeholder='Set password' value={password} onChange={(e)=> setPassword(e.target.value)} />
+        <button type='submit' className='btn btn-info d-block mx-auto'>Register</button>
+
+      </form>
+      </div>
+    </div>
+  </div>
+
+</>  )
 }
 
 export default Register
