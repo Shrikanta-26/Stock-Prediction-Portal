e0512d029de0924ba4debd7fd5d1cf629ce66fee
shrikanta <shrikantadutta108@gmail.com>
2026-01-29 21:03:00 +0600

    Dashboard building

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import StockPredictionSerializer
 
-# Create your views here.
+class StockPredictionAPIView(APIView):
+    def post(self, request):
+        serializer = StockPredictionSerializer(data=request.data)  # ✅ FIX
+
+        if serializer.is_valid():
+            ticker = serializer.validated_data['ticker']
+            return Response({'status': 'success', 'ticker': ticker},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,20 +1,44 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import axiosInstance from '../../axiosInstance'
 const Dashboard = () => {
+
+  const [ticker,setTicker] = useState('')
+
 
     useEffect(() => {
         const fetchProtectedData = async () => {
             try{
              const response = await axiosInstance.get('/protected-view')
-             console.log('Success: ',response.data)
             }catch(error){
             console.error(error)
             }
         }
         fetchProtectedData();
     }, [])
+
+ const handleSubmit =async (e) =>{
+    e.preventDefault();
+    try{
+      const response =await axiosInstance.post('/predict/',{
+        ticker:ticker
+      });
+      console.log(response.data)
+    }catch(error){
+      console.log('There was an error making the API request',error)
+    }
+}
+
   return (
-    <div className='text-light'>Dashboard</div>
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <form onSubmit={handleSubmit}>
+            <input type="text" className='form-control' placeholder='Enter Stock Ticker' onChange={(e)=> setTicker(e.target.value)} required/>
+            <button type='submit' className='btn btn-info mt-3'>See Prediction</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
