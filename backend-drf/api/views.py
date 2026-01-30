c2ac40253b0c783d58d0aaf22e5be4da1c4a2fdf
shrikanta <shrikantadutta108@gmail.com>
2026-01-30 18:39:38 +0600

    Plot final prediction graph

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import os
 from django.conf import settings
+from .utils import save_plot
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
 
 class StockPredictionAPIView(APIView):
     def post(self, request):
@@ -34,19 +37,93 @@ class StockPredictionAPIView(APIView):
             plt.switch_backend('AGG')
             plt.figure(figsize=(12, 5))
             plt.plot(df.Close, label='Closing Price') 
-            plt.title(f"Closing price of {ticker}")
+            plt.title(f"Closing Price of {ticker}")
             plt.xlabel('Days')
-            plt.ylabel('Close price')
+            plt.ylabel('Price')
             plt.legend()
             
             #Save the plot to a file
-
             plot_img_path = f'{ticker}_plot.png'
-            image_path=os.path.join(settings.MEDIA_ROOT, plot_img_path)
-            plt.savefig(image_path)
-            plt.close()
-            plot_img=settings.MEDIA_URL + plot_img_path
-            print(plot_img)
+            plot_img = save_plot(plot_img_path)
 
-            return Response({'status': 'success', 'plot_img':plot_img},status=status.HTTP_200_OK)
+           # 100 Days moving average
+            ma100 = df.Close.rolling(100).mean()
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price') 
+            plt.plot(ma100, 'r', label='100 DMA')
+            plt.title(f"100 Days Moving Average of {ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+            plot_img_path = f'{ticker}_100_dma.png'
+            plot_100_dma = save_plot(plot_img_path)
+
+            # 200 Days moving average
+            ma200 = df.Close.rolling(200).mean()
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price') 
+            plt.plot(ma100, 'r', label='100 DMA')
+            plt.plot(ma200, 'g', label='200 DMA')
+            plt.title(f"200 Days Moving Average of {ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+            plot_img_path = f'{ticker}_200_dma.png'
+            plot_200_dma = save_plot(plot_img_path)
+
+            # Splitting data into Training & Testing datasets
+            data_training = pd.DataFrame(df.Close[0:int(len(df)*0.7)])
+            data_testing = pd.DataFrame(df.Close[int(len(df)*0.7):int(len(df))])
+
+
+            #Scaling down the data between 0 and 1
+            scaler = MinMaxScaler(feature_range=(0,1))
+
+            # Load ML Model
+            model = load_model('stock_prediction_model.keras')
+            
+            #Preparing Test Data
+            past_100_days = data_training.tail(100)
+            final_df=pd.concat([past_100_days, data_testing], ignore_index=True)
+            input_data = scaler.fit_transform(final_df)
+
+            x_test = []
+            y_test = []
+
+            for i in range(100, input_data.shape[0]):
+                x_test.append(input_data[i-100: i])
+                y_test.append(input_data[i, 0])
+            x_test, y_test = np.array(x_test), np.array(y_test)
+            
+            # Making Predictions
+            y_predicted = model.predict(x_test)
+ 
+            # Revert the scaled prices to original price
+            y_predicted = scaler.inverse_transform(y_predicted.reshape(-1,1)).flatten()
+            y_test = scaler.inverse_transform(y_test.reshape(-1,1)).flatten()
+
+
+            # print('y_predicted==> ',y_predicted)   
+            # print('y_test==> ',y_test)   
+            # Plot the final prediction
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12, 5))
+            plt.plot(y_test,'b', label='Original Price') 
+            plt.plot(y_predicted, 'r', label='Predicted Price')
+            plt.title(f"Final Prediction for {ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+            plot_img_path = f'{ticker}_final_prediction.png'
+            plot_prediction = save_plot(plot_img_path) 
+
+            return Response({
+                'status': 'success', 
+                'plot_img':plot_img,
+                'plot_100_dma':plot_100_dma,
+                'plot_200_dma':plot_200_dma,
+                'plot_prediction':plot_prediction
+                },status=status.HTTP_200_OK)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import axiosInstance from "../../axiosInstance";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
- 
 
 const Dashboard = () => {
   const [ticker, setTicker] = useState("");
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
-  const [plot,setPlot] = useState()
+  const [plot, setPlot] = useState();
+  const [ma100, setMA100] = useState();
+  const [ma200, setMA200] = useState();
+  const [prediction, setPrediction] = useState();
+
   useEffect(() => {
     const fetchProtectedData = async () => {
       try {
@@ -30,10 +33,17 @@ const Dashboard = () => {
       console.log(response.data);
 
       //Set Plot
-   
-       const backendRoot = import.meta.env.VITE_BACKEND_ROOT
-       const plotUrl = `${backendRoot}${response.data.plot_img}`
-       setPlot(plotUrl)
+
+      const backendRoot = import.meta.env.VITE_BACKEND_ROOT;
+      const plotUrl = `${backendRoot}${response.data.plot_img}`;
+      const ma100 = `${backendRoot}${response.data.plot_100_dma}`;
+      const ma200 = `${backendRoot}${response.data.plot_200_dma}`;
+      const predictionUrl = `${backendRoot}${response.data.plot_prediction}`;
+
+      setPlot(plotUrl);
+      setMA100(ma100);
+      setMA200(ma200);
+      setPrediction(predictionUrl);
       if (response.data.error) {
         setError(response.data.error);
       }
@@ -71,9 +81,16 @@ const Dashboard = () => {
         {/* Print prediction plot */}
         <div className="prediction mt-5">
           <div className="p-3">
-            {plot && (
-              <img src={plot} style={{maxWidth: '100%'}}/>
-            )}
+            {plot && <img src={plot} style={{ maxWidth: "100%" }} />}
+          </div>
+          <div className="p-3">
+            {ma100 && <img src={ma100} style={{ maxWidth: "100%" }} />}
+          </div>
+          <div className="p-3">
+            {ma200 && <img src={ma200} style={{ maxWidth: "100%" }} />}
+          </div>
+          <div className="p-3">
+            {prediction && <img src={prediction} style={{ maxWidth: "100%" }} />}
           </div>
         </div>
       </div>
